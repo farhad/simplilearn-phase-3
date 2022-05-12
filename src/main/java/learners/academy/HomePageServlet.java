ff66bc60109397dcb61c76ffb3ae1b870cff03f2
Farhad <ifarhadfaghihi@gmail.com>
2022-05-11 21:15:50 -0600

    added initial run config

---
+++ src/main/java/learners/academy/HomePageServlet.java
@@ -0,0 +1,18 @@
+package learners.academy;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "", name = "HomePageServlet")
+public class HomePageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().print("Learner's Academy Home Page");
+    }
+}
